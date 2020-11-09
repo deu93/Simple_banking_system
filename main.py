41c7c3ab09f340da682e013ae1590447c69e7bb0
deu93 <deucalion93@ukr.net>
2020-11-09 14:27:19 +0200

    luhn algorithm

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ class Bank:
                 self.cardnum_generate()
                 self.cardpass_generate()
                 print("Your card has been created")
-                print(f"Your card number:\n{int(self.cardnum)}")
+                print(f"Your card number:\n{int(self.final_cardnum3)}")
                 print(f"Your card PIN:\n{self.cardpass}\n")
                 
             elif self.choice == "2":
                 self.user_cardnum_inpt = input("Enter your card number:\n")
                 self.user_cardpass_inpt = input("Enter your PIN:\n")
-                if self.cardnum == self.user_cardnum_inpt:
+                if self.final_cardnum3 == self.user_cardnum_inpt:
                     if self.cardpass == self.user_cardpass_inpt:
                         print("You have successfully logged in!\n")
                         self.card_menu()
@@ -49,14 +49,36 @@ class Bank:
         for x in range(9):
             self.card_second_half += str(random.randint(0, 9))
         self.checksum = random.randint(0, 9)
-        self.cardnum = self.card_first_half + str(self.card_second_half) + str(self.checksum)
-
+        self.cardnum = self.card_first_half + str(self.card_second_half) 
+        self.sum_cardnums = 0
+        self.cardnum2 = [int(x) for x in self.cardnum]
+        self.cardnum3 = [v for k,v in enumerate(self.cardnum2) if not k%2]
+        self.cardnum4 = [v for k, v in enumerate(self.cardnum2) if k%2]
+        self.final_cardnum = self.cardnum
+        self.final_cardnum3 = ""
+        self.cheksum_gen()
+        
+    
+    def cheksum_gen(self):
+        for x in self.cardnum3:
+                x = x * 2
+                if x >= 9:
+                    x -= 9
+                self.sum_cardnums += x
+        for x in self.cardnum4:
+            self.sum_cardnums += x
+        if self.sum_cardnums % 10 == 0:
+            self.final_cardnum3 = self.cardnum + "0"
+        else:
+            self.checksum_num = self.sum_cardnums % 10
+            self.checksum_num1 = 10 - self.checksum_num
+            self.final_cardnum3 = self.cardnum + str(self.checksum_num1)
+            
     def cardpass_generate(self):
         self.cardpass = ""
         for x in range(4):
             self.cardpass += str(random.randint(0, 9))
-
-
+    
+        
 my_bank = Bank()
 my_bank.menu()
-
